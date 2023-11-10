a0e9dd3f023bee0dde286731299875c26a05e699
Elmo Egers <uplaymedia@gmail.com>
2023-11-10 08:16:39 +0200

    Add GridView component

---
+++ components/GridView.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="col-grid">
+	  <section>
+		  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eros nunc, luctus id finibus eu, rhoncus vel diam. Fusce nec mi non neque accumsan sollicitudin. Vivamus ac vehicula nunc. Duis vehicula vulputate vulputate. Donec nec tempor metus. Sed consequat libero nec leo fermentum, ac tempor mauris semper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec consequat, arcu in iaculis gravida, lectus mi faucibus neque, id malesuada lacus eros eget sem. Integer facilisis bibendum dolor eget ornare. Suspendisse potenti.</p>
+	  </section>
+	  <section>Pellentesque a tellus neque. Nullam venenatis ultricies lacus, nec interdum turpis convallis eget. Fusce in tellus consectetur, fermentum sapien ut, rutrum arcu. Mauris vel mi id arcu varius blandit. Maecenas porttitor, ipsum eget dictum viverra, turpis sapien dapibus dui, id tincidunt massa lectus eget leo. Vestibulum non purus at mauris convallis scelerisque vitae sed sapien. Proin nibh tellus, posuere in viverra id, vestibulum at nibh. Duis congue non purus at placerat. Aliquam finibus eros eget leo sollicitudin vulputate. Integer semper mauris sed sem auctor tincidunt. Morbi iaculis enim elit, venenatis egestas sapien finibus id. Pellentesque pretium hendrerit lorem vel fringilla. Sed quis velit id turpis pharetra ultricies eu in ipsum. Pellentesque ornare ornare tortor. Aenean sodales semper erat sed consectetur. Aenean in sagittis sapien.</section>
+	  <img class="breakout" src="https://picsum.photos/1800/400" alt="Image" />
+	  <section>Pellentesque convallis risus est, ut sagittis orci aliquet ut. Mauris quis nulla maximus, rhoncus ante pellentesque, posuere nibh. Cras ullamcorper faucibus dui placerat bibendum. Donec et ex quis turpis tempor pretium nec non dui. Aliquam finibus nisi ac porttitor pellentesque. Etiam in erat quis elit elementum congue in ac velit. In egestas sollicitudin ultrices. Vivamus diam diam, dapibus eget leo nec, molestie luctus leo. Nullam mattis quis lorem eu iaculis. Nulla eget dolor sit amet eros blandit sollicitudin vitae ac turpis. Vestibulum dapibus lacus quis neque vestibulum, quis rhoncus velit auctor. Vestibulum commodo molestie tortor, sed blandit velit molestie non. Suspendisse et odio augue. Maecenas faucibus tortor quis fermentum sollicitudin. Quisque sodales hendrerit purus non imperdiet. Nullam ipsum urna, molestie vel nunc mollis, interdum ultricies nulla.</section>
+	  <section>Vivamus volutpat dui vel sodales dignissim. Phasellus ac nunc ut purus vestibulum accumsan non sit amet odio. Nullam et pretium tortor. Sed molestie nibh ut felis convallis suscipit. Curabitur ullamcorper quam tellus, laoreet molestie nibh varius eu. Aenean pellentesque mauris quis orci malesuada, quis bibendum risus viverra. Praesent at iaculis dolor. Nullam faucibus neque diam, quis aliquam purus ultricies vel.</section>
+	  <section class="full-w bg-amber-300">
+		  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eros nunc, luctus id finibus eu, rhoncus vel diam. Fusce nec mi non neque accumsan sollicitudin. Vivamus ac vehicula nunc. Duis vehicula vulputate vulputate. Donec nec tempor metus. Sed consequat libero nec leo fermentum, ac tempor mauris semper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec consequat, arcu in iaculis gravida, lectus mi faucibus neque, id malesuada lacus eros eget sem. Integer facilisis bibendum dolor eget ornare. Suspendisse potenti.</p>
+	  </section>
+	  <section class="bg-blue-300">
+		  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eros nunc, luctus id finibus eu, rhoncus vel diam. Fusce nec mi non neque accumsan sollicitudin. Vivamus ac vehicula nunc. Duis vehicula vulputate vulputate. Donec nec tempor metus. Sed consequat libero nec leo fermentum, ac tempor mauris semper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec consequat, arcu in iaculis gravida, lectus mi faucibus neque, id malesuada lacus eros eget sem. Integer facilisis bibendum dolor eget ornare. Suspendisse potenti.</p>
+	  </section>
+	  <section class="breakout-r">
+		  <div class="flex gap-2">
+			  <div>
+				  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eros nunc, luctus id finibus eu, rhoncus vel diam. Fusce nec mi non neque accumsan sollicitudin. Vivamus ac vehicula nunc. Duis vehicula vulputate vulputate. Donec nec tempor metus. Sed consequat libero nec leo fermentum, ac tempor mauris semper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec consequat, arcu in iaculis gravida, lectus mi faucibus neque, id malesuada lacus eros eget sem. Integer facilisis bibendum dolor eget ornare. Suspendisse potenti.</p>
+			  </div>
+			  <div class="bg-emerald-300">
+				  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eros nunc, luctus id finibus eu, rhoncus vel diam. Fusce nec mi non neque accumsan sollicitudin. Vivamus ac vehicula nunc. Duis vehicula vulputate vulputate. Donec nec tempor metus. Sed consequat libero nec leo fermentum, ac tempor mauris semper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec consequat, arcu in iaculis gravida, lectus mi faucibus neque, id malesuada lacus eros eget sem. Integer facilisis bibendum dolor eget ornare. Suspendisse potenti.</p>
+			  </div>
+		  </div>
+	  </section>
+	  <section class="breakout-l">
+		  <div class="flex gap-2">
+			  <div class="bg-emerald-300">
+				  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eros nunc, luctus id finibus eu, rhoncus vel diam. Fusce nec mi non neque accumsan sollicitudin. Vivamus ac vehicula nunc. Duis vehicula vulputate vulputate. Donec nec tempor metus. Sed consequat libero nec leo fermentum, ac tempor mauris semper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec consequat, arcu in iaculis gravida, lectus mi faucibus neque, id malesuada lacus eros eget sem. Integer facilisis bibendum dolor eget ornare. Suspendisse potenti.</p>
+			  </div>
+			  <div>
+				  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eros nunc, luctus id finibus eu, rhoncus vel diam. Fusce nec mi non neque accumsan sollicitudin. Vivamus ac vehicula nunc. Duis vehicula vulputate vulputate. Donec nec tempor metus. Sed consequat libero nec leo fermentum, ac tempor mauris semper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec consequat, arcu in iaculis gravida, lectus mi faucibus neque, id malesuada lacus eros eget sem. Integer facilisis bibendum dolor eget ornare. Suspendisse potenti.</p>
+			  </div>
+		  </div>
+	  </section>
+	  <section class="full-w-r">
+		  <div>
+			  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eros nunc, luctus id finibus eu, rhoncus vel diam. Fusce nec mi non neque accumsan sollicitudin. Vivamus ac vehicula nunc. Duis vehicula vulputate vulputate. Donec nec tempor metus. Sed consequat libero nec leo fermentum, ac tempor mauris semper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec consequat, arcu in iaculis gravida, lectus mi faucibus neque, id malesuada lacus eros eget sem. Integer facilisis bibendum dolor eget ornare. Suspendisse potenti.</p>
+		  </div>
+		  <div class="bg-emerald-300">
+			  <p>ALorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eros nunc, luctus id finibus eu, rhoncus vel diam. Fusce nec mi non neque accumsan sollicitudin. Vivamus ac vehicula nunc. Duis vehicula vulputate vulputate. Donec nec tempor metus. Sed consequat libero nec leo fermentum, ac tempor mauris semper. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec consequat, arcu in iaculis gravida, lectus mi faucibus neque, id malesuada lacus eros eget sem. Integer facilisis bibendum dolor eget ornare. Suspendisse potenti.</p>
+		  </div>
+	  </section>
+	  <p>Pellentesque a tellus neque. Nullam venenatis ultricies lacus, nec interdum turpis convallis eget. Fusce in tellus consectetur, fermentum sapien ut, rutrum arcu. Mauris vel mi id arcu varius blandit. Maecenas porttitor, ipsum eget dictum viverra, turpis sapien dapibus dui, id tincidunt massa lectus eget leo. Vestibulum non purus at mauris convallis scelerisque vitae sed sapien. Proin nibh tellus, posuere in viverra id, vestibulum at nibh. Duis congue non purus at placerat. Aliquam finibus eros eget leo sollicitudin vulputate. Integer semper mauris sed sem auctor tincidunt. Morbi iaculis enim elit, venenatis egestas sapien finibus id. Pellentesque pretium hendrerit lorem vel fringilla. Sed quis velit id turpis pharetra ultricies eu in ipsum. Pellentesque ornare ornare tortor. Aenean sodales semper erat sed consectetur. Aenean in sagittis sapien.</p>
+  </div>
+</template>
+
+<style>
+.col-grid {
+	@apply grid;
+	--col-padding: 2rem;
+	--contained-max-width: 75rem;
+	--breakout-max-width: 90rem;
+	--full-w: minmax(var(--col-padding), 1fr);
+	--breakout: minmax(0, calc((var(--breakout-max-width) - var(--contained-max-width)) / 2));
+	--contained: min(100% - (var(--col-padding) * 2), var(--contained-max-width));
+	grid-template-columns:
+		[full-w-start] var(--full-w)
+		[breakout-start] var(--breakout)
+		[contained-start] var(--contained) [contained-end]
+		var(--breakout) [breakout-end]
+		var(--full-w) [full-w-end];
+}
+
+.col-grid > * {
+	@apply py-3
+}
+
+.col-grid > :not(.breakout, .breakout-l, .breakout-r, .full-w, .full-w-l, .full-w-r),
+.full-w > :not(.breakout, .breakout-l, .breakout-r, .full-w, .full-w-l, .full-w-r) {
+	grid-column: contained;
+}
+
+/* Handle contained grid elements with a background color */
+.col-grid > :not(.breakout, .breakout-l, .breakout-r, .full-w, .full-w-r)[class*="bg-"],
+.full-w > :not(.breakout, .breakout-l, .breakout-r, .full-w, .full-w-r)[class*="bg-"] {
+	@apply px-4 py-3;
+}
+
+.col-grid > .breakout {
+	grid-column: breakout;
+}
+
+.col-grid > .breakout-l {
+	grid-column-start: breakout-start;
+	grid-column-end: contained-end;
+}
+
+.col-grid > .breakout-r {
+	grid-column-start: contained-start;
+	grid-column-end: breakout-end;
+}
+
+.col-grid > .full-w,
+.col-grid > .full-w-r,
+.col-grid > .full-w-l {
+	@apply grid;
+	grid-column: full-w;
+	grid-template-columns: inherit;
+}
+</style>
+<script setup lang="ts">
+</script>
